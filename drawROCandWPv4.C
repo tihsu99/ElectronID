e7f9fd410c4b1660eee89b751fcc0af46d33c59b
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-11-09 11:23:34 +0100

    drawROCandWPv4 in batch

--- drawROCandWPv4.C
+++ drawROCandWPv4.C
@@ -11,34 +11,26 @@
 #include "VarCut.hh"
 
 #include <iostream>
-using std::cout;
 
 // Draw barrel or endcap
-const bool drawBarrel = true;
 const int nROCs = 4;
 
 const bool verbose = true;
-
-// Files with signal and background trees (ideally the ntuples
-// that were used for TMVA optimization
-const TString fnameSignal = drawBarrel ? Opt::fnameSignalBarrel : Opt::fnameSignalEndcap;
-const TString signalTreeName = "electronTree";
-const TString fnameBackground = drawBarrel ? Opt::fnameBackgroundBarrel : Opt::fnameBackgroundEndcap;
-const TString backgroundTreeName = "electronTree";
+const TString dateTag = "2017-11-07";
 
 // Name TMVA output file that contains the pre-computed ROC, etc
 const TString tmvaFileNameBarrel[nROCs] = {
-  "./trainingData/training_results_barrel_pass1_20171030_200000/TMVA_training_results_barrel_pass1_20171030_200000.root",
-  "./trainingData/training_results_barrel_pass2_20171030_200000/TMVA_training_results_barrel_pass2_20171030_200000.root",
-  "./trainingData/training_results_barrel_pass3_20171030_200000/TMVA_training_results_barrel_pass3_20171030_200000.root",
-  "./trainingData/training_results_barrel_pass4_20171030_200000/TMVA_training_results_barrel_pass4_20171030_200000.root"
+  "./trainingData/training_results_barrel_pass1_" + dateTag + "/TMVA_training_results_barrel_pass1_" + dateTag + ".root",
+  "./trainingData/training_results_barrel_pass2_" + dateTag + "/TMVA_training_results_barrel_pass2_" + dateTag + ".root",
+  "./trainingData/training_results_barrel_pass3_" + dateTag + "/TMVA_training_results_barrel_pass3_" + dateTag + ".root",
+  "./trainingData/training_results_barrel_pass4_" + dateTag + "/TMVA_training_results_barrel_pass4_" + dateTag + ".root"
 };
 
 const TString tmvaFileNameEndcap[nROCs] ={
-  "./trainingData/training_results_endcap_pass1_20171030_200000/TMVA_training_results_endcap_pass1_20171030_200000.root",
-  "./trainingData/training_results_endcap_pass2_20171030_200000/TMVA_training_results_endcap_pass2_20171030_200000.root",
-  "./trainingData/training_results_endcap_pass3_20171030_200000/TMVA_training_results_endcap_pass3_20171030_200000.root",
-  "./trainingData/training_results_endcap_pass4_20171030_200000/TMVA_training_results_endcap_pass4_20171030_200000.root"
+  "./trainingData/training_results_endcap_pass1_" + dateTag + "/TMVA_training_results_endcap_pass1_" + dateTag + ".root",
+  "./trainingData/training_results_endcap_pass2_" + dateTag + "/TMVA_training_results_endcap_pass2_" + dateTag + ".root",
+  "./trainingData/training_results_endcap_pass3_" + dateTag + "/TMVA_training_results_endcap_pass3_" + dateTag + ".root",
+  "./trainingData/training_results_endcap_pass4_" + dateTag + "/TMVA_training_results_endcap_pass4_" + dateTag + ".root"
 };
 
 //
@@ -50,38 +42,22 @@ const int markerColorSet1 = kRed;
 const int markerStyleSet1 = 20;
 const TString legendSet1 = "WP_Veto";
 const int nWP = 1;
-const TString cutFileNamesBarrelSet1[nWP] = { 
-  "cut_repository/cuts_barrel_20171030_200000_WP_Veto.root"
-
-};
-const TString cutFileNamesEndcapSet1[nWP] = {
-  "cut_repository/cuts_endcap_20171030_200000_WP_Veto.root"
-
-};
+const TString cutFileNamesBarrelSet1[nWP] = {"cut_repository/cuts_barrel_" + dateTag + "_WP_Veto.root"};
+const TString cutFileNamesEndcapSet1[nWP] = {"cut_repository/cuts_endcap_" + dateTag + "_WP_Veto.root"};
 
 // Set 2
 const int markerColorSet2 = kOrange;
 const int markerStyleSet2 = 20;
 const TString legendSet2 = "WP_Loose";
-const TString cutFileNamesBarrelSet2[nWP] = { 
-  "cut_repository/cuts_barrel_20171030_200000_WP_Loose.root"
-
-};
-const TString cutFileNamesEndcapSet2[nWP] = {
-  "cut_repository/cuts_endcap_20171030_200000_WP_Loose.root"
-};
+const TString cutFileNamesBarrelSet2[nWP] = {"cut_repository/cuts_barrel_" + dateTag + "_WP_Loose.root"};
+const TString cutFileNamesEndcapSet2[nWP] = {"cut_repository/cuts_endcap_" + dateTag + "_WP_Loose.root"};
 
 // Set 3
 const int markerColorSet3 = kBlue;
 const int markerStyleSet3 = 20;
 const TString legendSet3 = "WP_Medium";
-const TString cutFileNamesBarrelSet3[nWP] = { 
-  "cut_repository/cuts_barrel_20171030_200000_WP_Medium.root"
-
-};
-const TString cutFileNamesEndcapSet3[nWP] = {
-  "cut_repository/cuts_endcap_20171030_200000_WP_Medium.root"
-};
+const TString cutFileNamesBarrelSet3[nWP] = {"cut_repository/cuts_barrel_" + dateTag + "_WP_Medium.root"};
+const TString cutFileNamesEndcapSet3[nWP] = {"cut_repository/cuts_endcap_" + dateTag + "_WP_Medium.root"};
 
 
 
@@ -89,39 +65,31 @@ const TString cutFileNamesEndcapSet3[nWP] = {
 const int markerColorSet4 = kGreen;
 const int markerStyleSet4 = 20;
 const TString legendSet4 = "WP_Tight";
-const TString cutFileNamesBarrelSet4[nWP] = { 
-  "cut_repository/cuts_barrel_20171030_200000_WP_Tight.root"
-};
-const TString cutFileNamesEndcapSet4[nWP] = {
-  "cut_repository/cuts_endcap_20171030_200000_WP_Tight.root"
-};
+const TString cutFileNamesBarrelSet4[nWP] = {"cut_repository/cuts_barrel_" + dateTag + "_WP_Tight.root"};
+const TString cutFileNamesEndcapSet4[nWP] = {"cut_repository/cuts_endcap_" + dateTag + "_WP_Tight.root"};
 
 // Cuts on expected missing hits are separate from VarCut cuts, tuned by hand.
 // These are summer 2016 values:
 const int missingHitsBarrel[Opt::nWP] = { 2, 1, 1, 1};
 const int missingHitsEndcap[Opt::nWP] = { 3, 1, 1, 1};
-const int *missingHitsCut = drawBarrel ? missingHitsBarrel : missingHitsEndcap;
 
 void bazinga (std::string message){
-  if (verbose)
-    std::cout<<message<<std::endl;
+  if (verbose) std::cout<<message<<std::endl;
 }
 
 // Forward declarations
 TTree *getTreeFromFile(TString fname, TString tname);
-void overlayWorkingPoints(TCanvas *c1, 
-			  TTree *signalTree, TTree *backgroundTree, 
-			  const TString *cutFileNames,
-			  int markerColor, int markerStyle, 
-			  TLegend *leg, const TString legendText);
-void   findEfficiencies(TTree *signalTree, TTree *backgroundTree,
+void overlayWorkingPoints(TCanvas *c1, TTree *signalTree, TTree *backgroundTree, 
+			  const TString *cutFileNames, int markerColor, int markerStyle, 
+			  TLegend *leg, const TString legendText, bool drawBarrel);
+void findEfficiencies(TTree *signalTree, TTree *backgroundTree,
 			float &effSignal, float &effBackground, 
-			VarCut *cutObject, int iWorkingPoint);
+			VarCut *cutObject, int iWorkingPoint, bool drawBarrel);
 //
 // Main function
 //
-void drawROCandWPv4(){
-  
+void drawROCandWPv4(bool drawBarrel){
+
   bazinga("Step #0");
   TCanvas *c1 = new TCanvas("c1","",10,10,600,600);
   //  c1->cd();
@@ -152,8 +120,7 @@ void drawROCandWPv4(){
   TFile *tmvaFile2 = new TFile(tmvaFileName2);
   TFile *tmvaFile3 = new TFile(tmvaFileName3);
   TFile *tmvaFile4 = new TFile(tmvaFileName4);
-  if( !tmvaFile1 || !tmvaFile2 || !tmvaFile3 || !tmvaFile4)
-    assert(0);
+  if(!tmvaFile1 || !tmvaFile2 || !tmvaFile3 || !tmvaFile4) assert(0);
   bazinga("Step #3");
   // 
   //  Draw the ROC curve
@@ -177,25 +144,14 @@ void drawROCandWPv4(){
   for(int iBin = 1; iBin <=100; ++iBin){
 
     if (drawBarrel){
-    // barrrel
-      if(1<=iBin && iBin<=75)
-	hROC->SetBinContent(iBin, hROC3->GetBinContent(iBin)) ;
-      else if(76 <= iBin && iBin<=89)
-	hROC->SetBinContent(iBin, hROC2->GetBinContent(iBin)) ;
-      else if(90 <=iBin && iBin<=100)
-	hROC->SetBinContent(iBin, hROC1->GetBinContent(iBin)) ;
+      if(1<=iBin && iBin<=75)         hROC->SetBinContent(iBin, hROC3->GetBinContent(iBin)) ;
+      else if(76 <= iBin && iBin<=89)	hROC->SetBinContent(iBin, hROC2->GetBinContent(iBin)) ;
+      else if(90 <=iBin && iBin<=100)	hROC->SetBinContent(iBin, hROC1->GetBinContent(iBin)) ;
+    } else {
+      if(1<=iBin && iBin<=70)        	hROC->SetBinContent(iBin, hROC3->GetBinContent(iBin)) ;
+      else if(71 <= iBin && iBin<=89) hROC->SetBinContent(iBin, hROC2->GetBinContent(iBin)) ;
+      else if(90 <=iBin && iBin<=100)	hROC->SetBinContent(iBin, hROC1->GetBinContent(iBin)) ;
     }
-    else {
-      // for endcap
-      if(1<=iBin && iBin<=70)
-	hROC->SetBinContent(iBin, hROC3->GetBinContent(iBin)) ;
-      else if(71 <= iBin && iBin<=89)
-      hROC->SetBinContent(iBin, hROC2->GetBinContent(iBin)) ;
-      else if(90 <=iBin && iBin<=100)
-	hROC->SetBinContent(iBin, hROC1->GetBinContent(iBin)) ;
-    }
-    // else 
-    //   printf("\nThis should never happen!");
   }
   
   bazinga("Step #5");
@@ -240,14 +196,20 @@ void drawROCandWPv4(){
   // Overlay the cuts
   //
 
+  // Files with signal and background trees (ideally the ntuples
+  // that were used for TMVA optimization
+  const TString fnameSignal = drawBarrel ? Opt::fnameSignalBarrel : Opt::fnameSignalEndcap;
+  const TString signalTreeName = Opt::signalTreeName;
+  const TString fnameBackground = drawBarrel ? Opt::fnameBackgroundBarrel : Opt::fnameBackgroundEndcap;
+  const TString backgroundTreeName = Opt::backgroundTreeName;
+
   // First find the TestTree for measuring efficiency and rejection
-  printf("\n Take true electrons from %s   tree %s\n", 
-	 fnameSignal.Data(), signalTreeName.Data());
+  printf("\n Take true electrons from %s   tree %s\n", fnameSignal.Data(), signalTreeName.Data());
   TTree *signalTree = getTreeFromFile( fnameSignal, signalTreeName);
   // Input background tree  
-  printf("\n Take background electrons from %s   tree %s\n", 
-	 fnameBackground.Data(), backgroundTreeName.Data());
+  printf("\n Take background electrons from %s   tree %s\n", fnameBackground.Data(), backgroundTreeName.Data());
   TTree *backgroundTree = getTreeFromFile( fnameBackground, backgroundTreeName);
+
   bazinga("Step #8");
   // Next, draw all working point sets
   if( 1 ){
@@ -255,17 +217,13 @@ void drawROCandWPv4(){
     TLegend *leg = new TLegend(0.15, 0.45, 0.5, 0.7);
     leg->SetFillStyle(0);
     leg->SetBorderSize(0);
-    overlayWorkingPoints(c1, signalTree, backgroundTree, cutFileNamesSet1, 
-			 markerColorSet1, markerStyleSet1, leg, legendSet1);
+    overlayWorkingPoints(c1, signalTree, backgroundTree, cutFileNamesSet1, markerColorSet1, markerStyleSet1, leg, legendSet1, drawBarrel);
     bazinga("Step #9");
-    overlayWorkingPoints(c1, signalTree, backgroundTree, cutFileNamesSet2, 
-			 markerColorSet2, markerStyleSet2, leg, legendSet2);
+    overlayWorkingPoints(c1, signalTree, backgroundTree, cutFileNamesSet2, markerColorSet2, markerStyleSet2, leg, legendSet2, drawBarrel);
     bazinga("Step #10");
-    overlayWorkingPoints(c1, signalTree, backgroundTree, cutFileNamesSet3,
-			 markerColorSet3, markerStyleSet3, leg, legendSet3);
+    overlayWorkingPoints(c1, signalTree, backgroundTree, cutFileNamesSet3, markerColorSet3, markerStyleSet3, leg, legendSet3, drawBarrel);
     bazinga("Step #11");
-    overlayWorkingPoints(c1, signalTree, backgroundTree, cutFileNamesSet4,  			   
-			 markerColorSet4, markerStyleSet4, leg, legendSet4);
+    overlayWorkingPoints(c1, signalTree, backgroundTree, cutFileNamesSet4, markerColorSet4, markerStyleSet4, leg, legendSet4, drawBarrel);
     bazinga("Step #12");
     leg->Draw("same");
     
@@ -273,10 +231,8 @@ void drawROCandWPv4(){
   
   
     // Save the figure into a file
-    TString outname = "figures/plot_ROCandWP_barrel_27july.png";
+    TString outname = (TString)"figures/plot_ROCandWP_" + (drawBarrel? "barrel" : "endcap") + ".png";
     bazinga("Step #13");
-    if( !drawBarrel )
-      outname = "figures/plot_ROCandWP_endcap_27july.png";
     c1->Print(outname);
     
     return;
@@ -286,17 +242,15 @@ void drawROCandWPv4(){
 TTree *getTreeFromFile(TString fname, TString tname){
 
   TFile *file = new TFile( fname );
-  TTree *tree     = (TTree*) file->Get(tname);
+  TTree *tree = (TTree*) file->Get(tname);
   
   return tree;
 }
 
 // Draw on a given canvas the full set of working points
-void   overlayWorkingPoints(TCanvas *c1, 
-			    TTree *signalTree, TTree *backgroundTree, 
-			    const TString *cutFileNames,
-			    int markerColor, int markerStyle, 
-			    TLegend *leg, const TString legendText){
+void   overlayWorkingPoints(TCanvas *c1, TTree *signalTree, TTree *backgroundTree, 
+			    const TString *cutFileNames, int markerColor, int markerStyle, 
+			    TLegend *leg, const TString legendText, bool drawBarrel){
 
 
   // Now loop over working points
@@ -304,16 +258,13 @@ void   overlayWorkingPoints(TCanvas *c1,
     
     // Load the working point from a ROOT file
     TFile *cutFile = new TFile(cutFileNames[iwp]);
-    if( !cutFile )
-      assert(0);
+    if(!cutFile) assert(0);
     VarCut *cutObject = (VarCut*)cutFile->Get("cuts");
-    if( !cutObject )
-      assert(0);
+    if(!cutObject) assert(0);
     
     // Compute the efficiencies
     float effSignal, effBackground;
-    findEfficiencies(signalTree, backgroundTree, effSignal, effBackground,
-		     cutObject, iwp);
+    findEfficiencies(signalTree, backgroundTree, effSignal, effBackground, cutObject, iwp, drawBarrel);
     printf("Computed eff for cut from %s, effS= %.4f effB= %.4f\n",
 	   cutFileNames[iwp].Data(), effSignal, effBackground);
     
@@ -327,8 +278,7 @@ void   overlayWorkingPoints(TCanvas *c1,
     // Add marker to the legend only once. Do not draw the legend here,
     // it is drawn in the main function later
     if( iwp == 0 ){
-      if( !leg )
-	assert(0);
+      if( !leg ) assert(0);
       leg->AddEntry(marker, legendText, "p");
     }
 
@@ -343,14 +293,9 @@ void   overlayWorkingPoints(TCanvas *c1,
 // Compute signal and background efficiencies for given cuts
 void findEfficiencies(TTree *signalTree, TTree *backgroundTree,
 		      float &effSignal, float &effBackground, VarCut *cutObject,
-		      int iWorkingPoint){
+		      int iWorkingPoint, bool drawBarrel){
 
-  TCut etaCut = "";
-  if( drawBarrel ){
-    etaCut = Opt::etaCutBarrel;
-  }else{
-    etaCut = Opt::etaCutEndcap;
-  }
+  TCut etaCut = drawBarrel ? Opt::etaCutBarrel : Opt::etaCutEndcap;
   TCut kinematicCuts = Opt::ptCut && etaCut;
 
   TCut preselectionCuts = kinematicCuts && Opt::otherPreselectionCuts;
@@ -360,6 +305,7 @@ void findEfficiencies(TTree *signalTree, TTree *backgroundTree,
  
   TCut selectionCuts = *(cutObject->getCut());
 
+  const int *missingHitsCut = drawBarrel ? missingHitsBarrel : missingHitsEndcap;
   TCut missingHits = TString::Format("expectedMissingInnerHits<=%d", missingHitsCut[iWorkingPoint]).Data();
   selectionCuts = selectionCuts && missingHits;
   printf("\nNOTE: the missing hits cuts are not taken from optimization, but are added by hand!\n\n");
