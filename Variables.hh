2434eb7c02df3e5fe9fa4393b2a8b57a5ff5ba2f
UAEDF-tomc <tom.cornelis@ua.ac.be>
2017-11-21 13:07:12 +0100

    trying with the scaled H/E

--- Variables.hh
+++ Variables.hh
@@ -20,18 +20,18 @@ namespace Vars {
   
   Variables *variables [nVariables] = {
     new Variables("full5x5_sigmaIetaIeta","full5x5_sigmaIetaIeta",'F',false),
-    new Variables("dEtaSeed"               ,"abs(dEtaSeed)"          ,'F',true),
+    new Variables("dEtaSeed"             ,"abs(dEtaSeed)"        ,'F',true),
     new Variables("dPhiIn"               ,"abs(dPhiIn)"          ,'F',true),
-    new Variables("hOverE"               ,"hOverE"               ,'F',false),
-    new Variables("relIsoWithEA"      ,"relIsoWithEA"      ,'F',false),
+    new Variables("hOverE"               ,"hOverEscaled"         ,'F',false),
+    new Variables("relIsoWithEA"         ,"relIsoWithEA"         ,'F',false),
     new Variables("ooEmooP"              ,"ooEmooP"              ,'F',false)
   };
   
   Variables *spectatorVariables [nSpectatorVariables] = {
-    new Variables("pt"   , "pt"   , 'F',   false),
-    new Variables("etaSC", "etaSC", 'F',   false),
-    new Variables("d0"                   ,"abs(d0)"              ,'F',true),
-    new Variables("dz"                   ,"abs(dz)"              ,'F',true),
+    new Variables("pt"                      ,"pt"                      ,'F',false),
+    new Variables("etaSC"                   ,"etaSC"                   ,'F',false),
+    new Variables("d0"                      ,"abs(d0)"                 ,'F',true),
+    new Variables("dz"                      ,"abs(dz)"                 ,'F',true),
     new Variables("expectedMissingInnerHits","expectedMissingInnerHits",'I',false)
   };
   
