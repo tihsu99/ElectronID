b436efca07937b334292a5f94bfffabb48c1d9a7
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-03-21 11:13:58 +0100

    for optional second argument

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -4,4 +4,8 @@ echo "Compiling..."
 echo
 echo
 g++ -O3 -I `root-config --incdir` -o $1 $1.C `root-config --libs` -lMinuit $ROOTSYS/lib/libTMVA.so -std=c++1y 2>&1 >/dev/null | head -n 35
-./$1
+if [[ -z $2 ]]; then
+  ./$1
+else
+  ./$1 $2
+fi
