c1fadcfc524f4488354f3b894f79ba69801cc009
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-03-21 11:14:46 +0100

    T2 scripts

---
+++ runEleIdTrainingBarrel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /user/tomc/eleIdTuning/CMSSW_9_2_10/src/ElectronID
+source $VO_CMS_SW_DIR/cmsset_default.sh
+eval `scram runtime -sh`
+root -b -q 'fourPointOptimization.C(true)'  &> barrel.log
