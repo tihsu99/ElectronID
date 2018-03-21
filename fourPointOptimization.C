09adfc01315fdbac7cf57c61c43763cd7b021df7
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-03-21 11:36:33 +0100

    new tuples

--- fourPointOptimization.C
+++ fourPointOptimization.C
@@ -8,7 +8,7 @@
 void fourPointOptimization(bool useBarrel){
 
   // Define source for the initial cut range
-  TString dateTag = "2017-11-16";
+  TString dateTag = "2018-03-18";
   TString startingCutMaxFileName        = "cuts_barrel_eff_0999_" + dateTag + ".root";
   if(!useBarrel) startingCutMaxFileName = "cuts_endcap_eff_0999_" + dateTag + ".root";
 
