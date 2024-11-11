7d47f077a906806e59af647c9a69ef0ce43226ca
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2024-11-11 21:54:19 +0100

    [compile] update compile setting

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -3,7 +3,8 @@ eval `scram runtime -sh`
 echo "Compiling..."
 echo
 echo
-g++ -O3 -I `root-config --incdir` -o $1 $1.C `root-config --libs` -lMinuit $ROOTSYS/lib/libTMVA.so -std=c++1y 2>&1 >/dev/null | head -n 35
+#g++ -O3 -I `root-config --incdir` -o $1 $1.C `root-config --libs` -lMinuit $ROOTSYS/lib/libTMVA.so -std=c++1y 2>&1 >/dev/null | head -n 35
+g++ -O3 -I `root-config --incdir` -o $1 $1.C `root-config --libs` -lMinuit $ROOTSYS/lib/libTMVA.so -std=c++17 2>&1 >/dev/null | head -n 35
 if [[ -z $2 ]]; then
   ./$1
 else
